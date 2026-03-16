345366482c7bcbf25c663703b4f48a7f5456575d
Zahira Adiah Safa <raraadiah6@gmail.com>
2026-03-16 21:56:41 +0700

    Tugas LKP 4

--- app/src/main/java/com/example/praktam2_2417051012/MainActivity.kt
+++ app/src/main/java/com/example/praktam2_2417051012/MainActivity.kt
@@ -7,11 +7,19 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material.icons.outlined.FavoriteBorder
 import androidx.compose.material3.Button
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
@@ -50,18 +58,42 @@ fun DaftarTravelScreen() {
 @Composable
 fun DetailScreen(travel: Travel) {
 
+    var isFavorite by remember { mutableStateOf(false) }
+
     Column(
-        modifier = Modifier
-            .fillMaxWidth()
+        modifier = Modifier.fillMaxWidth()
     ) {
 
-        Image(
-            painter = painterResource(id = travel.imageRes),
-            contentDescription = travel.nama,
-            modifier = Modifier
-                .fillMaxWidth()
-                .height(200.dp)
-        )
+        Box {
+
+            Image(
+                painter = painterResource(id = travel.imageRes),
+                contentDescription = travel.nama,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(200.dp),
+                contentScale = ContentScale.Crop
+            )
+
+            IconButton(
+                onClick = { isFavorite = !isFavorite },
+                modifier = Modifier
+                    .align(Alignment.TopEnd)
+                    .padding(8.dp)
+            ) {
+
+                Icon(
+                    imageVector = if (isFavorite)
+                        Icons.Filled.Favorite
+                    else
+                        Icons.Outlined.FavoriteBorder,
+
+                    contentDescription = "Favorite",
+
+                    tint = if (isFavorite) Color.Red else Color.White
+                )
+            }
+        }
 
         Spacer(modifier = Modifier.height(8.dp))
 
